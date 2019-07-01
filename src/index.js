0345add3fc23d65a2353cb159cf48df5da51714e
sage-nilton <nilton.rodrigues@sage.com>
2019-07-01 18:11:01 -0300

    poc

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import * as Scrivito from 'scrivito';
+
+const FaqWidget = Scrivito.provideWidgetClass('FaqWidget', {
+  attributes: {
+    question: 'string',
+    answer: 'html',
+  },
+});
+
+Scrivito.provideEditingConfig('FaqWidget', {
+  title: 'FaqWidget',
+  description: 'A frequently asked question.',
+
+  attributes: {
+    question: {
+      title: 'Question',
+      description: 'State the frequently asked question',
+    },
+    answer: {
+      title: 'Answer',
+      description: 'Provide an eloquent answer',
+    },
+  },
+
+  properties: ['question', 'answer'],
+  initialContent: {
+    question: "What is the question?",
+    answer: "And this is the answer.",
+  },
+});
+
+Scrivito.provideComponent('FaqWidget', ({ widget }) => {
+  return (
+    <div className='mt-2 mr-0 pt-0 pr-0 pb-3 pl-2 border-bottom'>
+      <Scrivito.ContentTag tag='h3' content={ widget } attribute='question' />
+      <Scrivito.ContentTag content={ widget } attribute='answer'/>
+    </div>
+  );
+});
+
+export default FaqWidget;
