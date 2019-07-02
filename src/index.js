05bc62ca1a211fb3fb48b6fe22086a898b7ebe64
sage-nilton <nilton.rodrigues@sage.com>
2019-07-01 22:34:02 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,42 +1,48 @@
 import * as React from 'react';
-import * as Scrivito from 'scrivito';
+//import * as Scrivito from 'scrivito';
 
-const FaqWidget = Scrivito.provideWidgetClass('FaqWidget', {
-  attributes: {
-    question: 'string',
-    answer: 'html',
-  },
-});
+// const FaqWidget = Scrivito.provideWidgetClass('FaqWidget', {
+//   attributes: {
+//     question: 'string',
+//     answer: 'html',
+//   },
+// });
 
-Scrivito.provideEditingConfig('FaqWidget', {
-  title: 'FaqWidget',
-  description: 'A frequently asked question.',
+// Scrivito.provideEditingConfig('FaqWidget', {
+//   title: 'FaqWidget',
+//   description: 'A frequently asked question.',
 
-  attributes: {
-    question: {
-      title: 'Question',
-      description: 'State the frequently asked question',
-    },
-    answer: {
-      title: 'Answer',
-      description: 'Provide an eloquent answer',
-    },
-  },
+//   attributes: {
+//     question: {
+//       title: 'Question',
+//       description: 'State the frequently asked question',
+//     },
+//     answer: {
+//       title: 'Answer',
+//       description: 'Provide an eloquent answer',
+//     },
+//   },
 
-  properties: ['question', 'answer'],
-  initialContent: {
-    question: "What is the question?",
-    answer: "And this is the answer.",
-  },
-});
+//   properties: ['question', 'answer'],
+//   initialContent: {
+//     question: "What is the question?",
+//     answer: "And this is the answer.",
+//   },
+// });
 
-Scrivito.provideComponent('FaqWidget', ({ widget }) => {
-  return (
-    <div className='mt-2 mr-0 pt-0 pr-0 pb-3 pl-2 border-bottom'>
-      <Scrivito.ContentTag tag='h3' content={ widget } attribute='question' />
-      <Scrivito.ContentTag content={ widget } attribute='answer'/>
-    </div>
-  );
-});
+// Scrivito.provideComponent('FaqWidget', ({ widget }) => {
+//   return (
+//     <div className='mt-2 mr-0 pt-0 pr-0 pb-3 pl-2 border-bottom'>
+//       <Scrivito.ContentTag tag='h3' content={ widget } attribute='question' />
+//       <Scrivito.ContentTag content={ widget } attribute='answer'/>
+//     </div>
+//   );
+// });
+
+function FaqWidget() {
+  return(<div>
+    <h1>Teste de Componente</h1>
+  </div>)
+}
 
 export default FaqWidget;
